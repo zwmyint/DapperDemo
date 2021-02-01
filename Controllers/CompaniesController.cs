3a7087ac1e5e5833f31ad9367992258f0e9d3385
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2021-02-01 21:24:30 +0800

    20210201-2124

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperDemo.Controllers
+{
+    public class CompaniesController : Controller
+    {
+
+        //
+    }
+}
---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        //
+    }
+}
